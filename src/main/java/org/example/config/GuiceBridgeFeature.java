f96af9096ba17a0f9195484af3091a94626129a2
Aleksandr Kovtoniuk <aleksandr.kovtoniuk@brightpattern.com>
2023-04-18 10:04:40 +0400

    cleanup

--- src/main/java/org/example/config/GuiceBridgeFeature.java
+++ src/main/java/org/example/config/GuiceBridgeFeature.java
@@ -1,7 +1,6 @@
 package org.example.config;
 
 import com.google.inject.Injector;
-import org.example.startup.StartupServiceListener;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
 import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
@@ -14,7 +13,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.ext.Provider;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Provider
 public class GuiceBridgeFeature implements Feature {
